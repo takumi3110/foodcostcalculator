3a8eec4ff7b307ae95bdbb421c91c6fbd3108d62
takumi3110 <takumi3110@garitto.jp>
2024-01-29 07:53:28 +0900

    add: loading animation

--- lib/view/start_up/create_account_page.dart
+++ lib/view/start_up/create_account_page.dart
@@ -4,6 +4,7 @@ import 'package:foodcost/utils/authentication.dart';
 import 'package:foodcost/utils/firestore/users.dart';
 import 'package:foodcost/utils/functionUtils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'dart:io';
 
 class CreateAccountPage extends StatefulWidget {
@@ -20,90 +21,114 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
   TextEditingController passController = TextEditingController();
   File? image;
 
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('新規登録')
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                const SizedBox(height: 50,),
-                GestureDetector(
-                  onTap: () async{
-                    var result = await FunctionUtils.getImageFromGallery();
-                    if (result != null) {
-                      setState(() {
-                        image = File(result.path);
-                      });
-                    }
-                  },
-                  child: CircleAvatar(
-                    foregroundImage: image == null ? null: FileImage(image!),
-                    radius: 40,
-                    child: const Icon(Icons.add),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: nameController,
-                      decoration: const InputDecoration(
-                        hintText: '名前'
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        hintText: 'メールアドレス'
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: passController,
-                      decoration: const InputDecoration(
-                        hintText: 'パスワード'
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50,),
-                ElevatedButton(
-                    onPressed: () async{
-                      // 入力されてない時は作動しない
-                      if (nameController.text.isNotEmpty && emailController.text.isNotEmpty && passController.text.isNotEmpty && image !=null) {
-                        var result = await Authentication.signUp(email: emailController.text, pass: passController.text);
-                        if (result is UserCredential) {
-                          var _result = await createAccount(result.user!.uid);
-                          if (_result == true) {
-                            Navigator.pop(context);
-                          }
-                        }
+      body: Stack(
+        children:[
+          SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 50,),
+                  GestureDetector(
+                    onTap: () async{
+                      var result = await FunctionUtils.getImageFromGallery();
+                      if (result != null) {
+                        setState(() {
+                          image = File(result.path);
+                        });
                       }
                     },
-                    child: const Text('アカウント作成'))
-              ],
+                    child: CircleAvatar(
+                      foregroundImage: image == null ? null: FileImage(image!),
+                      radius: 40,
+                      child: const Icon(Icons.add),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          hintText: '名前'
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          hintText: 'メールアドレス'
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: passController,
+                        decoration: const InputDecoration(
+                          hintText: 'パスワード'
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50,),
+                  ElevatedButton(
+                      onPressed: () async{
+                        // 入力されてない時は作動しない
+                        if (nameController.text.isNotEmpty && emailController.text.isNotEmpty && passController.text.isNotEmpty && image !=null) {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          var result = await Authentication.signUp(email: emailController.text, pass: passController.text);
+                          if (result is UserCredential) {
+                            var _result = await createAccount(result.user!.uid);
+                            if (_result == true) {
+                              Navigator.pop(context);
+                            }
+                          }
+                          setState(() {
+                            _isLoading = false;
+                          });
+                        }
+                      },
+                      child: const Text('アカウント作成'))
+                ],
+              ),
             ),
           ),
         ),
+          if (_isLoading)
+            const Opacity(
+              opacity: 0.8,
+              child: ModalBarrier(
+                  dismissible: false,
+                  color: Colors.black
+              ),
+            ),
+          if (_isLoading)
+            Center(
+              child: LoadingAnimationWidget.stretchedDots(color: Colors.white, size: 50),
+            ),
+    ]
       ),
     );
   }
--- lib/view/start_up/login_page.dart
+++ lib/view/start_up/login_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:foodcost/utils/authentication.dart';
-import 'package:foodcost/utils/firestore/users.dart';
 import 'package:foodcost/view/calendar/calendar_page.dart';
 import 'package:foodcost/view/start_up/create_account_page.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,84 +17,109 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
 
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  'まんまのじぇんこ',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 40.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(hintText: 'メールアドレス'),
+    return Stack(
+      children: [
+        Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    'まんまのじぇんこ',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 40.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(hintText: 'メールアドレス'),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: passController,
-                    decoration: const InputDecoration(hintText: 'パスワード'),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: passController,
+                      decoration: const InputDecoration(hintText: 'パスワード'),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                RichText(
-                    text: TextSpan(style: const TextStyle(color: Colors.black), children: [
-                  const TextSpan(text: 'アカウントを作成していない方は'),
-                  TextSpan(
-                      text: 'こちら',
-                      style: const TextStyle(color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const CreateAccountPage()));
-                        })
-                ])),
-                const SizedBox(
-                  height: 70,
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      var result =
-                          await Authentication.emailSignIn(email: emailController.text, password: passController.text);
-                      // resultがUserCredentialタイプだったらtrue
-                      if (result is UserCredential) {
-                        // if (result.user!.emailVerified == true) {
-                        //   var _result = await UserFirestore.getUser(result.user!.uid);
-                        //   if (_result == true) {
-                        //     Navigator.pushReplacement(
-                        //         context, MaterialPageRoute(builder: (context) => const CalendarPage()));
-                        //   }
-                        // } else {
-                        //   print('メール認証できませんでした。');
-                        // }
-                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const CalendarPage()));
-                      }
-                    },
-                    child: const Text('メールアドレスでログイン'))
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  RichText(
+                      text: TextSpan(style: const TextStyle(color: Colors.black), children: [
+                    const TextSpan(text: 'アカウントを作成していない方は'),
+                    TextSpan(
+                        text: 'こちら',
+                        style: const TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const CreateAccountPage()));
+                          })
+                  ])),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        var result =
+                            await Authentication.emailSignIn(email: emailController.text, password: passController.text);
+                        // resultがUserCredentialタイプだったらtrue
+                        if (result is UserCredential) {
+                          // if (result.user!.emailVerified == true) {
+                          //   var _result = await UserFirestore.getUser(result.user!.uid);
+                          //   if (_result == true) {
+                          //     Navigator.pushReplacement(
+                          //         context, MaterialPageRoute(builder: (context) => const CalendarPage()));
+                          //   }
+                          // } else {
+                          //   print('メール認証できませんでした。');
+                          // }
+                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const CalendarPage()));
+                          setState(() {
+                            _isLoading = false;
+                          });
+                        }
+                      },
+                      child: const Text('メールアドレスでログイン')
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+        if (_isLoading)
+          const Opacity(
+            opacity: 0.7,
+            child: ModalBarrier(
+              dismissible: false,
+              color: Colors.black
+            ),
+          ),
+        if (_isLoading)
+          Center(
+            child: LoadingAnimationWidget.stretchedDots(color: Colors.white, size: 50),
+          ),
+    ]
     );
   }
 }
