d81a64e7ec6ba4f6ca2dd9504837da442c3528a8
takumi3110 <takumi3110@garitto.jp>
2024-02-14 07:21:23 +0900

    update: main color change

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Mamma no Jenco.',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
           useMaterial3: true,
         ),
         home: const LoginPage()
